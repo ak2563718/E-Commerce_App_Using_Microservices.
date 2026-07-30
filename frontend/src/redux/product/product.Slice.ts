1b7da73c2d7c6dc7fc1c40e579da208ee03c97dc
ak2563718 <ak2563718@gmail.com>
2026-07-30 09:53:40 +0530

    product slice created

---
+++ frontend/src/redux/product/product.Slice.ts
@@ -0,0 +1,113 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createProduct, deleteProductbyId, getAllProducts, getProductbyId, getProductWithSlug, updateProductbyId } from "./product.Action";
+
+interface data{
+    product:any|undefined,
+    products:any[]|undefined,
+    message:string|null,
+    error:string|null,
+    loading:boolean,
+}
+
+const initialState:data ={
+    product:{},
+    products:[],
+    message:null,
+    error:null,
+    loading:false,
+}
+
+const productSlice = createSlice({
+    name:"product",
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+        // 1. create product extra-reducer
+        builder.addCase(createProduct.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        }).addCase(createProduct.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.message = action.payload.message;
+            state.product = action.payload.data;
+            state.products?.push(...action.payload.data);
+        }).addCase(createProduct.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed";
+        });
+
+        // 2. get all products extra -reducer
+        builder.addCase(getAllProducts.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        }).addCase(getAllProducts.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.message = action.payload.message;
+            state.products = action.payload.data;
+        }).addCase(getAllProducts.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed";
+        });
+
+        // 3. get product by id extra -reducer
+        builder.addCase(getProductbyId.pending,(state)=>{
+            state.loading = true;
+            state.message = null;
+            state.error = null;
+        }).addCase(getProductbyId.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.message = action.payload.message;
+            state.product = action.payload.data;
+        }).addCase(getProductbyId.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed";
+        });
+
+        // 4. get product with slug
+        builder.addCase(getProductWithSlug.pending,(state)=>{
+            state.loading = true;
+            state.message = null;
+            state.error = null;
+        }).addCase(getProductWithSlug.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.message = action.payload.message;
+            state.product = action.payload.data;
+        }).addCase(getProductWithSlug.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed";
+        });
+
+        // 5. update product by Id
+        builder.addCase(updateProductbyId.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        }).addCase(updateProductbyId.fulfilled,(state,action)=>{
+            state.loading = true;
+            state.message = action.payload.message;
+            state.products = state.products?.map((p)=>p.id===action.payload.data.id?
+                                action.payload.data:p)
+        }).addCase(updateProductbyId.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed"; 
+        });
+
+        // 6. delete product by Id
+        builder.addCase(deleteProductbyId.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        }).addCase(deleteProductbyId.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.message = action.payload.message;
+            state.products = state.products?.filter((p)=>p.id !==action.payload.data.id)
+        }).addCase(deleteProductbyId.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload??"failed";
+        })
+    }
+});
+
+export default productSlice.reducer;
