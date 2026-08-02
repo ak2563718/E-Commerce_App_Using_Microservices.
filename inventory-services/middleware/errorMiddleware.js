f089f4fadbf70116f52b5df8442fc853a1a89973
ak2563718 <ak2563718@gmail.com>
2026-08-02 17:02:05 +0530

    Inventory service setup complete

---
+++ inventory-services/middleware/errorMiddleware.js
@@ -0,0 +1,8 @@
+export const errorMiddleware =(err, req, res, next)=>{
+    const statusCode = err.statusCode;
+    res.status(statusCode).json({
+        message:err.message,
+        success:false,
+    })
+    console.log(err)
+}
