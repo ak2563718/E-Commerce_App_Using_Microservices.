431198725a0662e54383b568a322a683cc8ece44
ak2563718 <ak2563718@gmail.com>
2026-08-03 23:49:52 +0530

    Product listing added

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import ProductListing from '@/component/dashboard/ProductListing'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <ProductListing/>
+    </div>
+  )
+}
+
+export default page
