8cf8c99962b9bd0fd9e6cf291886228d08a7b65e
ak2563718 <ak2563718@gmail.com>
2026-08-09 21:05:57 +0530

    client added to each page

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import AdminPage from '@/component/admin/AdminPage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <AdminPage/>
+    </div>
+  )
+}
+
+export default page
--- frontend/src/component/admin/AdminPage.tsx
+++ frontend/src/component/admin/AdminPage.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useRef, useCallback, useEffect } from 'react'
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
