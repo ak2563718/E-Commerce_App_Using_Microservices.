5a5d9195d158481d704b8a13e113257bf0af419d
ak2563718 <ak2563718@gmail.com>
2026-08-14 17:01:08 +0530

    cart services updated

--- cart-services/middleware/authMiddleware.js
+++ cart-services/middleware/authMiddleware.js
@@ -1,1 +1,23 @@
-expor
+import jwt from 'jsonwebtoken'
+export const authMiddleware = async(req, res, next)=>{
+     try {
+        let token;
+        if(req.headers.authorization || req.headers.authorization.startsWith("Bearer ")){
+            token = req.headers.authorization.split(" ")[1]
+        }
+        if(!token){
+            res.status(401).json({
+                message:"token not found",
+                success:false,
+            })
+        }
+        const decode = await jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decode;
+        next();
+     } catch (error) {
+        res.status(500).json({
+            message:"Internal server Error!",
+            success:false,
+        })
+     }
+}
