104b1778b3dd20bb3eb48e725beafff0817e9c93
ak2563718 <ak2563718@gmail.com>
2026-09-12 11:11:16 +0530

    auth service fully updated

--- auth-services/routes/auth.Routes.js
+++ auth-services/routes/auth.Routes.js
@@ -2,11 +2,10 @@ import express from 'express';
 import { auth_CheckSession, auth_forgotPassword, auth_refresh_AccessToken, auth_resetPassword, authLogin, authLogout, authRegister, authverifyEmail, createRole, sellerSignup } from '../controllers/auth.Controller.js';
 import { adminMiddleware, authMiddleware } from '../middleware/authMiddleware.js';
 import { loginLimiter, singupLimiter } from '../config/rateLimiting.js';
-import { createSellerSignup, updateSellerStatus } from '../controllers/sellerController.js';
+import { createSellerLogin, createSellerLogout, createSellerSession, createSellerSignup, updateSellerStatus } from '../controllers/sellerController.js';
 const router = express.Router();
 // user routes
 router.post('/register',singupLimiter,authRegister)
-// router.post('/seller-register',authMiddleware,sellerSignup)
 router.get('/verify-email',authverifyEmail)
 router.post('/login',authLogin)
 router.get('/logout',authLogout)
@@ -17,6 +16,9 @@ router.patch('/reset-password',auth_resetPassword)
 
 // seller route
 router.post('/seller/register', createSellerSignup)
+router.get('/seller/check-session',createSellerSession)
+router.post('/seller/login',createSellerLogin)
+router.get('/seller/logout',createSellerLogout)
 router.patch('/seller/status/:id',updateSellerStatus)
 
 // role create routes
