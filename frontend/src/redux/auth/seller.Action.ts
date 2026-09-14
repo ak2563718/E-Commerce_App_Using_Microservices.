0df43b1f83625a2d7805acd137da2205b43859b4
ak2563718 <ak2563718@gmail.com>
2026-09-14 22:03:40 +0530

    more api added

--- frontend/src/redux/auth/seller.Action.ts
+++ frontend/src/redux/auth/seller.Action.ts
@@ -4,6 +4,10 @@ interface logindata{
     email:string,
     password:string,
 }
+interface updateData{
+    id:string
+    status:"APPROVED"|"REJECTED"
+}
 // 1. signup api for seller 
 export const sellerSignup = createAsyncThunk<any, any, {rejectValue:string}>(
     'post/sellersignup',
@@ -48,6 +52,44 @@ export const sellerLogout = createAsyncThunk<any, void, {rejectValue:string}>(
     async(_, {rejectWithValue})=>{
         try {
             const { data } = await axios.get('http://localhost:6001/api/auth/seller/logout',{
+                headers:{'Content-Type':"application/json"},
+                withCredentials:true,
+            })
+            return data;
+        } catch (error) {
+            if(axios.isAxiosError(error)){
+                return rejectWithValue(error.response?.data.message)
+            }
+            return rejectWithValue("something went wrong")
+        }
+    }
+)
+
+// 4. check seller session 
+export const sellerCheckSession = createAsyncThunk<any, any, { rejectValue:string}>(
+    'get/checksession',
+    async(_, { rejectWithValue })=>{
+        try {
+            const { data } = await axios.get('http://localhost:6001/api/auth/seller/check-session',{
+                headers:{'Content-Type':"application/json"},
+                withCredentials:true,
+            })
+            return data;
+        } catch (error) {
+            if(axios.isAxiosError(error)){
+                return rejectWithValue(error.response?.data.message)
+            }
+            return rejectWithValue("something went wrong")
+        }
+    }
+)
+
+// 5. update seller status
+export const updateSellerStatus = createAsyncThunk<any, updateData,{rejectValue:string}>(
+    'patch/sellerStatus',
+    async({id,status},{rejectWithValue})=>{
+        try {
+            const { data }= await axios.patch(`http://localhost:6001/api/auth/seller/status/${id}`,status,{
                 headers:{'Content-Type':"application/json"},
                 withCredentials:true,
             })
